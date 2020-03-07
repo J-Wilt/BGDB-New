b9164a7d8b8f2ee74f1e449a217bc86fe896fd90
EC2 Default User <ec2-user@ip-172-31-37-222.us-east-2.compute.internal>
2020-03-07 06:08:24 +0000

    AWS Bucketeer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   
   #amazon s3
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
